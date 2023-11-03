0d4754e58c9ffd85c2dc5d5532a064a4fd6cc0e6
Jan <ja.dorn@web.de>
2023-11-03 13:39:39 +0100

    declare color variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#F9F4DA',
+				'secondary': '#F38BA3',
+				'accent': '#ED203D',
+				'background': '#0F0D0E',
+				'background-accent': '#241F20',
+			}
+		},
 	},
 	plugins: [],
 }
